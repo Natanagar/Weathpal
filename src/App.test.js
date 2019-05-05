4115154a887bea54bd9a27a2b8896f229cf58baf
Ciemna_noc <agarkowa@gmail.com>
2019-05-06 00:01:12 +0200

    first snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import Enzyme, {shallow, render, mount} from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+//configuration Enzyme
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({adapter: new Adapter()});
+
+describe("component App", () => {
+  it("component was rendered without mistakes", () => {
+    const defaultProps = {};
+    const component = shallow(<App {...defaultProps}/>);
+    expect(component).toMatchSnapshot();
+  });
+});
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
