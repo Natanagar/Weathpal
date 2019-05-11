9b0bdd15560a652bae3a7acbe2f35fd26247de7c
Nataliia Agarkova <ciemna_noc@Nataliias-MacBook-Pro.local>
2019-05-11 15:57:04 +0200

    adding from Pixer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Api from "./components/Api/index";
 import "./App.css";
 
 const App = () => {
-  const [data, putData] = useState({});
+  const [currency, putCurrency] = useState({});
 
   //ask to get first data
 
@@ -13,18 +13,24 @@ const App = () => {
     const api = new Api();
     const endpoint = keyData.endpoint;
     const key = keyData.key;
-    api.getData(endpoint, key).then(res => res.data).then(json => putData(json.rates)).catch(error => console.log(error));
+    api.getData(endpoint, key).then(res => {
+      const result = res.data.rates;
+      console.log(result)
+      putCurrency(result);
+    })
+    .catch(error => console.log(error));
   };
   //useEffect as didMount
-  useEffect(async data => (
-    const result = await getDataFromFixer();
-    console.log(result)
-  ));
-
-  return (<div className="App">
+  useEffect(data => {
+    const result = getDataFromFixer();
+  } ,[]);
+  
+  return(<div className="App">
     Currency exchange
     <label htmlFor="currency"/>
     <form id="currency">
+    
+      <div>Result {Object.values(currency)}</div>
       <ul/>
     </form>
   </div>);
